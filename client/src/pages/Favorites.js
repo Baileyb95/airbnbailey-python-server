e2aa9097c7e497277619e5bf679049edb1d0ec31
Baileyb95 <108492331+Baileyb95@users.noreply.github.com>
2023-11-04 04:02:32 -0400

    favorites done

--- client/src/pages/Favorites.js
+++ client/src/pages/Favorites.js
@@ -8,7 +8,6 @@ const Favorites = () => {
     fetch(`http://127.0.0.1:5000/favorites/${localStorage.getItem('id')}`)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         setFavorites(data);
       })
       .catch((error) => {
@@ -25,13 +24,9 @@ const Favorites = () => {
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         // After successful deletion, you should update the state to remove the deleted favorite
         setFavorites(favorites.filter((favorite) => favorite.id !== listingId));
       })
-        .then(() => {
-            window.location.reload();
-        })
       .catch((error) => {
         console.error('Error deleting property favorite:', error);
       });
@@ -41,18 +36,21 @@ const Favorites = () => {
     <>
       <Header />
       <div className="favorites">
-        <div>Favorites</div>
+        <h1>Favorites</h1>
         {favorites.map((favorite) => (
           <div key={favorite.id} className="favorite">
-            <h2>Favorite: {favorite.listing?.title}</h2>
-            <a>Image: <img src={favorite.listing?.image_url} alt="Listing" /></a>
-            <p>Description: {favorite.listing?.description}</p>
-            <p>Address: {favorite.listing?.address}</p>
-            <p>City: {favorite.listing?.city}</p>
-            <p>State: {favorite.listing?.state}</p>
-            <p>Zip Code: {favorite.listing?.zip_code}</p>
-            <p>Country: {favorite.listing?.country}</p>
-            <p>Price: {favorite.listing?.price}</p>
+            <h2>Favorite: {favorite.title}</h2>
+            {favorite.image_url && (
+              <div>
+                <p>Image: <img src={favorite.image_url} alt="Listing" /></p>
+              </div>
+            )}
+            <p>Description: {favorite.description}</p>
+            <p>Address: {favorite.address}</p>
+            <p>City: {favorite.city}</p>
+            <p>State: {favorite.state}</p>
+            <p>Zip Code: {favorite.zip_code}</p>
+            <p>Price: {favorite.price}</p>
             <button onClick={() => handleDeleteFavorite(favorite.id)}>Delete</button>
           </div>
         ))}
