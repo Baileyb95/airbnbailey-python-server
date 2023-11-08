3c3c717ae8a1eb134b62183a13070f43f2121286
Baileyb95 <108492331+Baileyb95@users.noreply.github.com>
2023-11-08 16:49:35 -0500

    deploy?

--- client/src/pages/DashBoard.js
+++ client/src/pages/DashBoard.js
@@ -17,41 +17,42 @@ const DashboardPage = () => {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="flex flex-col items-center">
-        <p className='text-3xl font-semibold text-center mb-4'>Dashboard</p>
-        <div className="grid grid-cols-2 grid-rows- gap-4">
-          <Link to="/rentals">
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:shadow-lg">
-              View Rentals
+    <div className="flex justify-center items-center min-h-screen bg-blue-100">
+      <div className="w-full max-w-md p-4 shadow-lg border border-gray-300 rounded bg-white-100">
+        <div className="flex flex-col items-center">
+          <p className='text-3xl font-semibold text-center mb-4'>Dashboard</p>
+          <div className="grid grid-cols-2 gap-4">
+            <Link to="/rentals">
+              <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:shadow-lg">
+                View Rentals
+              </button>
+            </Link>
+            <Link to="/bookings">
+              <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:shadow-lg">
+                View Bookings
+              </button>
+            </Link>
+            <Link to="/manage-listings">
+              <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:shadow-lg">
+                Manage Listings
+              </button>
+            </Link>
+            <Link to="/favorites">
+              <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:shadow-lg">
+                View Favorites
+              </button>
+            </Link>
+            <Link to="/account-settings">
+              <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:shadow-lg">
+                Account Settings
+              </button>
+            </Link>
+            <button
+              onClick={handleLogout}
+              className="bg-red-500 text-white px-4 py-2 rounded-lg hover:shadow-lg">
+              Logout
             </button>
-          </Link>
-          <Link to="/bookings">
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:shadow-lg">
-              View Bookings
-            </button>
-          </Link>
-          <Link to="/manage-listings">
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:shadow-lg">
-              Manage Listings
-            </button>
-          </Link>
-          <Link to="/favorites">
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:shadow-lg">
-              View Favorites
-            </button>
-          </Link>
-          <Link to="/account-settings">
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:shadow-lg">
-              Account Settings
-            </button>
-          </Link>
-          <button
-            onClick={handleLogout}
-            className="bg-red-500 text-white px-4 py-2 rounded-lg hover:shadow-lg"
-          >
-            Logout
-          </button>
+          </div>
         </div>
       </div>
     </div>
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const HomePage = () => {
     return (
         <div className="h-screen flex flex-col justify-center items-center bg-gradient-to-b from-blue-500 to-blue-700 text-white">
-            <h1 className="text-4xl font-bold mb-4">Welcome to AirbnBailey</h1>
+            <h1 className="text-4xl font-bold mb-4">Welcome to Air BnBailey</h1>
             <p className="text-lg mb-4">Discover your next adventure with us.</p>
             <div className="space-x-4">
                 <Link to="/login">
